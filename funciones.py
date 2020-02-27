d2920c4d38c9cd5a2e42ed0b3b74f42d7a377590
Anehi Karely Mayorquin Higuera <31140367+anehik@users.noreply.github.com>
2020-02-27 08:57:20 -0600

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,7 +1,33 @@
-
 # -- ------------------------------------------------------------------------------------ -- #
 # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
 # -- archivo: funciones.py - procesamiento de datos
 # -- mantiene: Francisco ME
 # -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME_PRUEBA
 # -- ------------------------------------------------------------------------------------ -- #
+import pandas as pd
+# -- --------------------------------------------------- FUNCION: Leer archivo de entrada -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# --
+def f_leer_archivo(param_archivo):
+    """
+    Parameters
+    ----------
+    param_archivo : str : nombre de archivo a leer
+    Returns
+    -------
+    df_data : pd.DataFrame : con informacion contenida en archivo leido
+    Debugging
+    ---------
+    param_archivo = 'archivo_tradeview_1.xlsx'
+    """
+    # Leer archivo de datos y guardarlo en un DataFrame
+    df_data = pd.read_excel('archivos/' + param_archivo, sheet_name='Hoja1')
+    # convertir en minusculas el nombre de las columnas
+    df_data.columns = [list(df_data.columns)[i].lower()
+                       for i in range(0, len(df_data.columns))]
+    # Asegurar que ciertas columnas son del tipo numerico
+    numcols = ['s/l', 't/p', 'commission', 'openprice', 'closeprice', 'profit', 'size', 'swap',
+               'taxes', 'order']
+    df_data[numcols] = df_data[numcols].apply(pd.to_numeric)
+
+    return df_data
--- principal.py
+++ principal.py
@@ -5,3 +5,5 @@
 # -- mantiene: Francisco ME
 # -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME_PRUEBA
 # -- ------------------------------------------------------------------------------------ -- #
+import funciones as fn
+datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
