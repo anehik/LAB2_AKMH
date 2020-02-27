e72267d3a24852a810ef12bb0e40a580baef1b10
Anehi Karely Mayorquin Higuera <31140367+anehik@users.noreply.github.com>
2020-02-27 07:30:02 -0600

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,7 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: datos.py - datos generales para uso en el proyecto
+# -- mantiene: Francisco ME
+# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME_PRUEBA
+# -- ------------------------------------------------------------------------------------ -- #
---
+++ funciones.py
@@ -0,0 +1,7 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: funciones.py - procesamiento de datos
+# -- mantiene: Francisco ME
+# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME_PRUEBA
+# -- ------------------------------------------------------------------------------------ -- #
