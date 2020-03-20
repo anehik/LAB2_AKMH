77ef205eefb395f75a47a7c8e938dcffd0971a73
Anehi Karely Mayorquin Higuera <31140367+anehik@users.noreply.github.com>
2020-03-19 22:51:38 -0600

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,8 +1,8 @@
 # -- ------------------------------------------------------------------------------------ -- #
 # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
-# -- archivo: funciones.py - procesamiento de datos
+# -- archivo: funciones.py - para procesamiento de datos
 # -- mantiene: Francisco ME
-# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME_PRUEBA
+# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME
 # -- ------------------------------------------------------------------------------------ -- #
 import pandas as pd
 # -- --------------------------------------------------- FUNCION: Leer archivo de entrada -- #
@@ -12,22 +12,97 @@ def f_leer_archivo(param_archivo):
     """
     Parameters
     ----------
-    param_archivo : str : nombre de archivo a leer
+    pa´´
+    ram_archivo : str : nombre de archivo a leer
     Returns
     -------
     df_data : pd.DataFrame : con informacion contenida en archivo leido
     Debugging
     ---------
-    param_archivo = 'archivo_tradeview_1.xlsx'
+    param_archivo = 'archivo_tradeview_1.csv'
     """
-    # Leer archivo de datos y guardarlo en un DataFrame
-    df_data = pd.read_excel('archivos/' + param_archivo, sheet_name='Hoja1')
-    # convertir en minusculas el nombre de las columnas
-    df_data.columns = [list(df_data.columns)[i].lower()
-                       for i in range(0, len(df_data.columns))]
-    # Asegurar que ciertas columnas son del tipo numerico
-    numcols = ['s/l', 't/p', 'commission', 'openprice', 'closeprice', 'profit', 'size', 'swap',
-               'taxes', 'order']
+    df_data = pd.read_csv('archivos/' + param_archivo)
+    df_data.columns = [i.lower() for i in list(df_data.columns)]
+    numcols = ['s/l', 't/p', 'commission', 'openprice', 'closeprice', 'profit', 'size', 'swap', 'taxes', 'order']
     df_data[numcols] = df_data[numcols].apply(pd.to_numeric)
-
     return df_data
+# -- ------------------------------------------------------ FUNCION: Pips por instrumento -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# -- calcular el tamaño de los pips por instrumento
+def f_pip_size(param_ins):
+    """
+    Parameters
+    ----------
+    param_ins : str : nombre de instrumento
+    Returns
+    -------
+    Debugging
+    -------
+    param_ins = 'usdjpy'
+    """
+    # encontrar y eliminar un guion bajo
+    # inst = param_ins.replace('_', '')
+    # transformar a minusculas
+    inst = param_ins.lower()
+    # lista de pips por instrumento
+    pips_inst = {'usdjpy': 100, 'gbpjpy': 100, 'eurjpy': 100, 'cadjpy': 100,
+                 'chfjpy': 100,
+                 'eurusd': 10000, 'gbpusd': 10000, 'usdcad': 10000, 'usdmxn': 10000,
+                 'audusd': 10000, 'nzdusd': 10000,
+                 'usdchf': 10000,
+                 'eurgbp': 10000, 'eurchf': 10000, 'eurnzd': 10000, 'euraud': 10000,
+                 'gbpnzd': 10000, 'gbpchf': 10000, 'gbpaud': 10000,
+                 'audnzd': 10000, 'nzdcad': 10000, 'audcad': 10000,
+                 'xauusd': 10, 'xagusd': 10, 'btcusd': 1}
+    return pips_inst[inst]
+# -- ------------------------------------------------------ FUNCION: Convertir a datetime -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# -- convertir los datos de fechas en formato datetime
+def f_columnas_tiempos(param_data):
+    """
+    Parameters:
+    param_data : str : nombre del archivo a leer.
+    Return : pd DataFrame :
+    Debugging
+    --------
+    param_data = datos
+    """
+    # Convertir las columnas de closetime y opentime con to_datetime
+    param_data['closetime'] = pd.to_datetime(param_data['closetime'])
+    param_data['opentime'] = pd.to_datetime(param_data['opentime'])
+    # Tiempo transcurrido de una operación
+    param_data['tiempo'] = [(param_data.loc[i, 'closetime'] - param_data.loc[i, 'opentime']).delta / 1e9
+                            for i in range(0, len(param_data['closetime']))]
+    return param_data
+# -- ------------------------------------------------------------ FUNCION: Columnas Pips --- #
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Calcula los pips acumulados y el profit acumulado.
+def f_columnas_pips(datos):
+    datos['pips_acm'] = [(datos.closeprice[i]-datos.openprice[i])*f_pip_size(datos.symbol[i]) for i in range(len(datos))]
+    datos['pips_acm'][datos.type=='sell'] *= -1
+    datos['profit_acm'] = datos['profit'].cumsum()
+    return datos
+# -- ------------------------------------------------------ FUNCION: Estadísticas básicas -- #
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Calcula algunas estadísticas entre las operaciones generadas.
+def f_estadisticas_ba(datos):
+    return pd.DataFrame({
+        'Ops totales': [len(datos['order']), 'Operaciones totales'],
+        'Ganadoras': [len(datos[datos['pips_acm']>=0]), 'Operaciones ganadoras'],
+        'Ganadoras_c': [len(datos[(datos['type']=='buy') & (datos['pips_acm']>=0)]), 'Operaciones ganadoras de compra'],
+        'Ganadoras_s': [len(datos[(datos['type']=='sell') & (datos['pips_acm']>=0)]), 'Operaciones ganadoras de venta'],
+        'Perdedoras': [len(datos[datos['pips_acm'] < 0]), 'Operaciones perdedoras'],
+        'Perdedoras_c': [len(datos[(datos['type']=='buy') & (datos['pips_acm']<0)]), 'Operaciones perdedoras de compra'],
+        'Perdedoras_s': [len(datos[(datos['type']=='sell') & (datos['pips_acm']<0)]), 'Operaciones perdedoras de venta'],
+        'Mediana_profit': [datos['profit'].median(), 'Mediana de rendimeintos de las operaciones'],
+        'Mediana_pips': [datos['pips_acm'].median(), 'Mediana de pips de las operaciones'],
+        'r_efectividad': [len(datos[datos['pips_acm']>=0])/len(datos['order']),
+                          'Operaciones Totales Vs Ganadoras Totales'],
+        'r_proporcion': [len(datos[datos['pips_acm']>=0])/len(datos[datos['pips_acm'] < 0]),
+                            'Ganadoras Totales Vs Perdedoras Totales'],
+        'r_efectividad_c': [len(datos[(datos['type']=='buy') & (datos['pips_acm']>=0)])/len(datos['order']),
+                            'Totales Vs Ganadoras Compras'],
+        'r_efectividad_v': [len(datos[(datos['type']=='sell') & (datos['pips_acm']>=0)])/len(datos['order']),
+                            'Totales Vs Ganadoras Ventas']
+    })
+
--- principal.py
+++ principal.py
@@ -1,9 +1,12 @@
-
 # -- ------------------------------------------------------------------------------------ -- #
 # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
-# -- archivo: principal.py - layout general del proyecto
+# -- archivo: principal.py - flujo principal del proyecto
 # -- mantiene: Francisco ME
-# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME_PRUEBA
+# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME
 # -- ------------------------------------------------------------------------------------ -- #
 import funciones as fn
+
 datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
+fn.f_pip_size(param_ins='usdjpy')
+datos = fn.f_columnas_datos(param_data=datos)
+datos_col = fn.f_columnas_pips(param_data=datos)
